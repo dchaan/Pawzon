779fddcfe8bfde303a5d09ddb22fd1eaeef8d5e7
David Chan <dchaan1@gmail.com>
2021-11-28 21:49:31 -0800

    Add reviews reducer

---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_REVIEWS, RECEIVE_REVIEW,  REMOVE_REVIEW} from "../actions/review_action";
+
+const reviewsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+
+  switch(action.type) {
+    case RECEIVE_REVIEWS:
+      return action.reviews;
+    case RECEIVE_REVIEW:
+      nextState[action.review.id] = action.review;
+      return nextState;
+    case REMOVE_REVIEW:
+      delete nextState[action.review.id];
+      return nextState;
+    default:
+      return state;
+  };
+}
+
+export default reviewsReducer;
