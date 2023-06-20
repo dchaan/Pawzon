716601deabbb9c4512635e68d8ea556284fb64e9
David Chan <dchaan1@gmail.com>
2023-06-20 09:24:17 -0700

    idk

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = query ? Product.search(query.downcase) : Product.all
+    @products = query.empty? ? Product.search(query.downcase) : Product.all
     render :index
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_023023) do
+ActiveRecord::Schema[7.0].define(version: 2021_12_01_182351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,16 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_023023) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum"
+    t.string "checksum", null: false
     t.datetime "created_at", precision: nil, null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -92,5 +85,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_023023) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
